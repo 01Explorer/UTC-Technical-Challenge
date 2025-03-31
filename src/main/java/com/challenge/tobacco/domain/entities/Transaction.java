c8291a99c605c1a4efca281d6a39c1726508559e
Gabriel <gabrielsantostca@gmail.com>
2025-03-31 01:26:39 -0300

    refactor: changed id type

--- src/main/java/com/challenge/tobacco/domain/entities/Transaction.java
+++ src/main/java/com/challenge/tobacco/domain/entities/Transaction.java
@@ -18,7 +18,7 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    private Integer id;
+    private Long id;
 
     @CreationTimestamp
     @Column(name = "created_at")
@@ -34,11 +34,11 @@ public class Transaction {
         this.bundle = bundle;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
